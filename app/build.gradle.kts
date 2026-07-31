5989cb56ee158ba8c6e2149897122087f438821e
Sachin Taware <sachintaware.2551988@gmail.com>
2026-07-31 10:28:21 +0530

    v2.5.0: add <queries> for com.tailscale.ipn (Android 11+ package visibility)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hermes.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.4.1"
+        versionCode = 11
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
