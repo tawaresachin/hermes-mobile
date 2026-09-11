9ab12b808e13729c08fdc408b3df8d73da4bebf2
Sachin Taware <sachintaware.2551988@gmail.com>
2026-09-12 01:18:19 +0530

    v0.0.27: versionCode 27 / versionName 0.0.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hermes.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 25
-        versionName = "0.0.25"
+        versionCode = 27
+        versionName = "0.0.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
