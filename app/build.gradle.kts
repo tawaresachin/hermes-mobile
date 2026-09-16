83b9d21cd4f92382072f0c755c8dcf8fb51cef53
Sachin Taware <vare.2551988@gmail.com>
2026-09-16 22:07:43 +0530

    bump version to 0.0.56

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.hermes.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 54
-        versionName = "0.0.54"
+        versionCode = 56
+        versionName = "0.0.56"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
