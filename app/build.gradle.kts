ef87433c67e5ff2b7365328a2182d527548ab54a
Sachin Taware <vare.2551988@gmail.com>
2026-09-16 22:07:43 +0530

    revert version to 0.0.54: tag v0.0.55 was premature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.hermes.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 55
-        versionName = "0.0.55"
+        versionCode = 54
+        versionName = "0.0.54"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
